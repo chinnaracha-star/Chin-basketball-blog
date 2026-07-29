c41c864e929791f7d3ffd16d8988a12c122f2091
Chindurex <chinnaracha@gmail.com>
2026-07-29 21:39:51 +0700

    feat: add Express server with profiles endpoint; update package.json and vercel.json for routing

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/profiles", (req, res) => {
+  res.status(200).json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
+});
+
+export default app;
